063e506b4588e2d8ccddae0a9b30fb5e95dc176b
Xy123z <puskarbanerjee5@gmail.com>
2025-11-06 09:44:46 +0530

    Add functionality for word replacement feature

--- src/components/find_n_repl.jsx
+++ src/components/find_n_repl.jsx
@@ -1,12 +1,19 @@
  import { useState } from 'react'
  export default function Functionality(props){
      const [word, setWord] = useState('')
+     const [word1, setWord1] = useState('')
   const changeWord = (event) => {
     setWord(event.target.value);
+  }
+  const changeWord1 = (event) => {
+    setWord1(event.target.value);
   }
   const countWord = (str,word) => {
       const words = str.toLowerCase().split(/\s+/)
       return words.filter( w => w === word.toLowerCase()).length
+}
+const replaceWord = (str,word1,word2) => {
+  str.toLowerCase().replaceAll(word1.toLowerCase(),word2.toLowerCase())
 }
      if(props.value === "no_of_occur"){
 
@@ -25,10 +32,13 @@
          return(
              <div className={`card ${props.mode === "dark" ? "text-bg-dark" : ""}`} style={{width: "18rem"}}>
   <div className="card-body">
-    <h5 className="card-title mb-4">find and replace</h5>
+    <h5 className="mb-4 card-title">find and replace</h5>
    <textarea className={`mb-4 form-control bg-${props.mode === 'dark' ? 'dark' : 'light'}`} id="exampleFormControlTextarea1" rows="1" cols="10" placeholder="enter the word to be replaced" value={word} onChange={changeWord}></textarea>
-    <textarea className={`mb-4 form-control bg-${props.mode === 'dark' ? 'dark' : 'light'}`} id="exampleFormControlTextarea1" rows="1" cols="10" placeholder="enter the new word" value={word} onChange={changeWord}></textarea>
-    <button type="button" className={`mt-4 btn btn-outline-${props.mode === "dark" ? "light" : "dark"}`} onClick={() => {props.removeFunctionality('')}}>close</button>
+    <textarea className={`mb-4 form-control bg-${props.mode === 'dark' ? 'dark' : 'light'}`} id="exampleFormControlTextarea1" rows="1" cols="10" placeholder="enter the new word" value={word1} onChange={changeWord1}></textarea>
+    <div className="d-flex ">
+    <button type="button" className={`mt-4 btn btn-outline-${props.mode === "dark" ? "light" : "dark"}`} onClick={() => {replaceWord(props.textVal,word,word1)}}>submit</button>
+  <button type="button" className={`ms-4 mt-4 btn btn-outline-${props.mode === "dark" ? "light" : "dark"}`} onClick={() => {props.removeFunctionality('')}}>close</button>
+  </div>
   </div>
 </div>
         )
